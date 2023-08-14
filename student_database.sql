696958eaed0dc9e23fd948dc98466315a3c7e7aa
Ks428 <83123502+Ks428@users.noreply.github.com>
2023-08-15 00:07:43 +0530

    Update student_database.sql

--- student_database.sql
+++ student_database.sql
@@ -1,21 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 23, 2021 at 07:31 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Database: `student_database`
@@ -64,8 +54,7 @@ INSERT INTO `student` (`id`, `name`, `rollno`, `standard`, `pcont`, `city`, `ima
 (3, 'Rahul Singh', 1, 11, '09999999994', 'Mumbai', 0x696d6167655f70726f63657373696e6732303230303431302d32343732312d63626a776d765f325f3830783132302e706e67),
 (4, 'Riya', 4, 12, '089654xxxx4', 'Delhi', 0x646f776e6c6f6164202831292e706e67);
 
---
--- Indexes for dumped tables
+
 --
 
 --
@@ -81,7 +70,6 @@ ALTER TABLE `student`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
 --
 
 --
@@ -97,6 +85,4 @@ ALTER TABLE `student`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
